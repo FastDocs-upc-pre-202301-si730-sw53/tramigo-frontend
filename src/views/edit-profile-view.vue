b5a75484d9505b5039c78670ef585d2d0db1ec1d
Alonso Moisés García Murga <u202110867@upc.edu.pe>
2023-05-25 10:16:42 -0500

    feature/ Edit User view
    Se añadio la vista Editar Perfil

---
+++ src/views/edit-profile-view.vue
@@ -0,0 +1,38 @@
+<template>
+    <Card>
+      <template #title> Editar Perfil</template>
+    </Card>
+    <div class="format">
+        <div>
+            <label for="username">Username</label>
+            <InputText id="username" v-model="value" aria-describedby="username-help" />
+        </div>
+        <div>
+            <label for="email">Email</label>
+            <InputText id="email" v-model="value" aria-describedby="email-help" />
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <InputText id="password" v-model="value" aria-describedby="password-help" />
+        </div>
+        <Button type="submit" label="Submit" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Edit-Profile-Component'
+}
+</script>
+
+<style scoped>
+.format{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+    gap: 1rem;
+}
+</style>
