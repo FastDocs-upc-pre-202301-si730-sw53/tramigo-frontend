b2541d69f8e150cac1c08333bbbe32b76b490041
Alonso Moisés García Murga <u202110867@upc.edu.pe>
2023-05-25 10:14:06 -0500

    feature/ Procedure view
    Se añadió la vista Mis tramites

--- src/views/procedure-view.vue
+++ src/views/procedure-view.vue
@@ -1,38 +1,31 @@
 <template>
-    <div class="container">
-        <h1>Mis Tramites</h1>
-        <Card>
-        <template #title> Solicitud de Servicio </template>
-        <template #content>
-            <h2>Se pidio servicio de internet</h2>
-        </template>
-        <template #footer>
-            <Button label="En Proceso" icon="pi-spinner" severity="warning"/>
-            <Button label="Info" icon="pi-info-circle" severity="info"/>
-        </template>
-        </Card>
-    </div>
+    <Card>
+      <template #title> Mis Tramites</template>
+    </Card>
+    <DataTable :value="Procedure" tableStyle="min-width: 50rem">
+      <Column field="id" header="N."></Column>
+      <Column field="name" header="Name"></Column>
+      <Column field="description" header="Description"></Column>
+      <Column field="status" header="Status"></Column>
+    </DataTable>
 </template>
 
 <script>
+import { FakeUsersService } from '@/services/fake-user.service'
   export default{
     name: 'ProcedureComponent',
     data() {
-        return {
-            value: ''
-        }
+      return {
+        Procedure:[],
+        fakeUserService: new FakeUsersService(),
+      }
+    },
+    beforeMount(){
+        this.Procedure = this.fakeUserService.getProcedure()
+        console.log(this.Procedure)
     }
   }
 </script>
 
 <style scoped>
-.container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-.p-card{
-   background-color:  #A3D9A5;
-}
 </style>
